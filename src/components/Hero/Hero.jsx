e3b5569487e2f495f50b04f3cf779f88fdd2773a
k-gourav <code2change.gourav@gmail.com>
2024-07-06 23:39:09 +0530

    Updated hero section

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,13 +1,19 @@
 import React from 'react'
+import banner from '../../assets/images/herobanner.svg'
 import style from './Hero.module.css'
+import headerStyle from '../Header/Header.module.css'
 
 function Hero() {
   return (
     <main>
         <div className={style.hero__container}>
             <div className={style.hero__content}>
-                <h2></h2>
-
+                <h2>Discover the new way of reading blogs</h2>
+                <p>Blogspace helps you explore, organize and share your favorite blogs.</p>
+                <button id={style.cta__btn} className={headerStyle.getstarted__btn}>Get Started for free</button>
+            </div>
+            <div className={style.hero__banner}>
+                <img className={style.banner} src={banner} alt="hero-banner-showing-depicts-reading" />
             </div>
         </div>
     </main>
